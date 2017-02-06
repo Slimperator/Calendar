d4a70f50fd8df46af2329091cc41e5de38040fce
Slimperator <slimperator@gmail.com>
2017-02-07 00:27:47 +0300

    Update DB Connector Interface
    -update getNotes args

--- OrganizerCalendar/src/main/java/com/Calendar/DataBaseConnector/IDBConnector.java
+++ OrganizerCalendar/src/main/java/com/Calendar/DataBaseConnector/IDBConnector.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Created by Владимир on 19.01.2017.
  */
 public interface IDBConnector {
-    ArrayList<String> getNotes();
+    ArrayList<String> getNotes(ArrayList<String> params);
     void editNote(String note);
     void addNote(ArrayList<String> notes);
     void deleteNote(String note);
