50903dcf180c5e94a7869b5e9b513403bc78a05c
Slimperator <slimperator@gmail.com>
2017-02-06 22:53:56 +0300

    Add Calendar connector

---
+++ OrganizerCalendar/src/main/java/com/Calendar/DataBaseConnector/CalendarConnector.java
@@ -0,0 +1,37 @@
+package com.Calendar.DataBaseConnector;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ * Created by Владимир on 19.01.2017.
+ */
+public class CalendarConnector implements IDBConnector{
+    private Connection connection;
+
+    public CalendarConnector(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    @Override
+    public ArrayList<String> getNotes() {
+        return null;
+    }
+
+    @Override
+    public void editNote(String note) {
+
+    }
+
+    @Override
+    public void addNote(ArrayList<String> notes) {
+
+    }
+
+    @Override
+    public void deleteNote(String note) {
+
+    }
+}
