e2d9766683db8504e9bc4133aef8ac08911ccf3f
Slimperator <slimperator@gmail.com>
2017-02-06 22:51:10 +0300

    Add Data Base Connector Interface

---
+++ OrganizerCalendar/src/main/java/com/Calendar/DataBaseConnector/IDBConnector.java
@@ -0,0 +1,13 @@
+package com.Calendar.DataBaseConnector;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Владимир on 19.01.2017.
+ */
+public interface IDBConnector {
+    ArrayList<String> getNotes();
+    void editNote(String note);
+    void addNote(ArrayList<String> notes);
+    void deleteNote(String note);
+}
