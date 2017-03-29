8af27b1b4bcc877d316ac19305601204e427dbe0
Slimperator <slimperator@gmail.com>
2017-03-29 04:06:02 +0300

    add users roles enum for spring security

---
+++ OrganizerCalendar/src/main/java/com/calendar/server/security/spring/UserRoleEnum.java
@@ -0,0 +1,15 @@
+package com.calendar.server.security.spring;
+
+/**
+ * Created by Владимир on 29.03.2017.
+ */
+public enum UserRoleEnum {
+
+    ADMIN,
+    USER,
+    ANONYMOUS;
+
+    UserRoleEnum() {
+    }
+
+}
